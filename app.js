17b915c519f8a3eafd37648338392b5041f1c997
Daniel Lawson <daniellawson9999@gmail.com>
2018-02-11 18:06:44 -0600

    added important stuff

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('*',(req,res) => {
+  const ip = res.socket.remoteAddress;
+  let language = req.headers["accept-language"];
+  language = language.slice(0,language.indexOf(','));
+  let env = req.headers["user-agent"];
+  env = env.slice(env.indexOf('(')+1);
+  env = env.slice(0,env.indexOf(')'));
+  const information = {
+    ipaddress: ip,
+    language: language,
+    software: env
+  };
+  res.send(information);
+  //console.log(req.headers);
+});
+
+app.listen(port);
